27783515b9e764eed25a15d4e615572ca9bfa979
Dinh Han <dinhhant9@gmail.com>
2021-08-25 13:29:45 +0700

    Update thoi tiet

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const request = require('request');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 3000;
@@ -15,9 +16,23 @@ app.get("/", async (req, res) => {
     res.send("Han pro");
 });
 
-app.get("/tin-tuc", async (req, res) => {
-    let feed = await parser.parseURL('https://vnreview.vn/feed/-/rss/home');
-    res.json(feed);
+app.post("/tin-tuc", async (req, res) => {
+    res.send("Han pro");
+});
+
+app.post("/thoi-tiet", async (req, res) => {
+    request.get('https://api.openweathermap.org/data/2.5/weather?q=Quy%20Nhon&lang=vi&appid=cca17b8c0e40be90211345a325ea8151', function (error, response, body) {
+        console.log('body:', JSON.parse(body));
+        let objBody = JSON.parse(body)
+        let objRes = {
+            data: [
+                {
+                    message: "Thời tiết Quy Nhơn: " + (objBody.main.temp - 273.15).toFixed(0) + "°C, " + objBody.weather[0].description
+                }
+            ]
+        }
+        res.json(objRes);
+    });
 });
 
 app.post("/gia-xang", async (req, res) => {
