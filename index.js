27a14cd5f3bb3f8dace17d03bd9063b36e4e35e7
dinhhant9 <30476635+dinhhant9@users.noreply.github.com>
2021-11-11 23:08:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json())
 app.options('*', cors());
 
 const crawler = require("./crawler")
@@ -23,7 +24,7 @@ app.post("/tin-tuc", async (req, res) => {
         data: []
     }
     feed.items.forEach((item, index) => {
-        if (index < 10) objRes.data.push({message: item.title})
+        if (index < 10) objRes.data.push({ message: item.title })
     })
 
     res.json(objRes);
@@ -33,14 +34,8 @@ app.post("/thoi-tiet", async (req, res) => {
     request.get('https://api.openweathermap.org/data/2.5/weather?q=Quy%20Nhon&lang=vi&appid=cca17b8c0e40be90211345a325ea8151', function (error, response, body) {
         console.log('body:', JSON.parse(body));
         let objBody = JSON.parse(body)
-        let objRes = {
-            data: [
-                {
-                    message: "Thời tiết Quy Nhơn: " + (objBody.main.temp - 273.15).toFixed(0) + "°C, " + objBody.weather[0].description
-                }
-            ]
-        }
-        res.json(objRes);
+        let message = "Thời tiết Quy Nhơn: " + (objBody.main.temp - 273.15).toFixed(0) + "°C, " + objBody.weather[0].description
+        sendResponse(res, message)
     });
 });
 
@@ -65,34 +60,105 @@ app.post("/luong", async (req, res) => {
         b = b.set("date", 4);
     }
 
-    
-
     let diff = a.diff(b, 'days')
 
-    let objRes = {
-        data: [
-            {
-                message: "Còn " + Math.abs(diff) + " ngày nữa nhận lương (" + b.format("DD/MM/YYYY") + ")"
-            }
-        ]
-    }
-    res.json(objRes);
+    let message = "Còn " + Math.abs(diff) + " ngày nữa nhận lương (" + b.format("DD/MM/YYYY") + ")"
+    sendResponse(res, message)
 });
 
 app.post("/gia-xang", async (req, res) => {
     crawler.LayGiaXang().then(result => {
-        let objRes = {
-            data: [
-                {
-                    message: "Giá xăng 92: " + result.gia_xang_92 + " vnđ\nGiá xăng 95: " + result.gia_xang_95 + " vnđ"
-                }
-            ]
-        }
-        res.json(objRes);
+        let message = "Giá xăng 92: " + result.gia_xang_92 + " vnđ\nGiá xăng 95: " + result.gia_xang_95 + " vnđ"
+        sendResponse(res, message);
     }).catch(() => {
-        res.status(500).send("Da co loi xay ra");
+        sendResponse(res, "Lỗi không lấy được giá xăng");
     })
 });
+
+app.post("/ai", async (req, res) => {
+    let message = req.body.senderMessage.trim().toLowerCase()
+
+    if (['noel', 'giáng sinh', 'xmas', 'christmas'].includes(message)) {
+        var a = moment();
+        var b = moment([a.year(), 11, 24]);
+        let diff = b.diff(a, 'days')
+
+        if (diff < 0) {
+            b = moment([a.year() + 1, 11, 24]);
+        }
+
+        diff = b.diff(a, 'days')
+
+        let message = "Sắp tới Noel: " + Math.abs(diff) + " ngày nữa"
+        sendResponse(res, message)
+        return
+    }
+
+    if (['năm mới', 'hết năm', 'tết tây', 'new year'].includes(message)) {
+        var a = moment();
+        var b = moment([a.year() + 1, 0, 1]);
+        let diff = b.diff(a, 'days')
+
+        let message = "Còn " + Math.abs(diff) + " ngày nữa nết năm " + a.year()
+        sendResponse(res, message)
+        return
+    }
+
+    if (['cảm ơn', 'thank you', 'tạm biệt', 'goodbye'].includes(message)) {
+        let message = "Cảm ơn bạn"
+        sendResponse(res, message)
+        return
+    }
+
+    if (['xin chào', 'hello', 'hi', 'chào bạn', 'chào'].includes(message)) {
+        let message = "Xin chào bạn"
+        sendResponse(res, message)
+        return
+    }
+
+    if (['không?', 'k?', 'ko?'].includes(message)) {
+        let messageArr = ['Có', 'Không']
+        let random = messageArr[Math.floor(Math.random() * messageArr.length)]
+        sendResponse(res, random)
+        return
+    }
+
+    if (['ai là', 'ai laf', 'ai la', 'who is'].includes(message)) {
+        let messageArr = ['Nai', 'Đon', 'Dương', 'Back', 'Quâng', 'Vương', 'Dance', 'Duck', 'Tiếng']
+        let random = messageArr[Math.floor(Math.random() * messageArr.length)]
+        sendResponse(res, "Là ông " + random)
+        return
+    }
+
+    if (['xin phép', 'xin phep'].includes(message)) {
+        let messageArr = ['Nai', 'Đon', 'Dương', 'Back', 'Quâng', 'Vương', 'Dance', 'Duck', 'Tiếng']
+        let random = messageArr[Math.floor(Math.random() * messageArr.length)]
+        sendResponse(res, "Djt cm ông " + random)
+        return
+    }
+
+    if (['làm gì', 'làm gi', 'lam gì', 'lam gi'].includes(message)) {
+        let messageArr = ['rửa đít', 'ỉa', 'rặn', 'lứng', 'sc', 'nhậu', 'chơi đỉ', 'massage', 'bắn', 'chụp hình nude', 'cởi chuồng', 'sóc lọ', 'cống hiến cho công ty lớn', 'chat sẽ', 'xem phim heo', 'xem sẽ gầy', 'tét đít', 'mân mê con cu', 'cạo lông chim', 'bóp dái', 'chụp ảnh cu', 'bôi dầu gió vào cu']
+        let random = messageArr[Math.floor(Math.random() * messageArr.length)]
+        sendResponse(res, "Đang " + random)
+        return
+    }
+
+
+    res.send("Han pro");
+
+});
+
+function sendResponse(res, message) {
+    let objRes = {
+        data: [
+            {
+                message: message
+            }
+        ]
+    }
+    res.json(objRes);
+}
 
 app.listen(port, () => {
     console.log("Server started on http://localhost:" + port);
